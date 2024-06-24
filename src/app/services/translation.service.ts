bb7777222ce335073e13650ddaac8ea6a621f816
Khanh <phamngockhanh2723@gmail.com>
2024-06-24 17:02:34 +0700

    update

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,21 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
-  private apiUrl = "http://localhost:4200/translate"; // Cập nhật địa chỉ của Spring Boot API
+  private apiUrl = "http://localhost:8080/convert'; // Cập nhật cổng nếu cần
 
   constructor(private http: HttpClient) { }
 
-  translateText(input: string, sourceLang: string, targetLang: string): Observable<any> {
-    const body = {
-      input: input,
-      sourceLang: sourceLang,
-      targetLang: targetLang
-    };
-    return this.http.post(this.apiUrl, body);
+  translateText(input: string, inputType: string, outputType: string): Observable<string> {
+    const body = { input, inputType, outputType };
+
+    return this.http.post<string>(this.apiUrl, body, { responseType: 'text' as 'json'})
   }
 }
